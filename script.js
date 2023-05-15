c779f0d941ed6140de565fd4261dc288c837ae6b
Sakshi Mishra <102748498+sakshimishra19@users.noreply.github.com>
2023-05-15 19:39:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+// Select the element with the ID of "menu-btn" and assign it to the variable "menu"
 let menu = document.querySelector('#menu-btn');
+// Select the element with the class "navbar" within the element with the class "header" and assign it to the variable "navbar"
 let navbar = document.querySelector('.header .navbar');
 
+// Add an event listener to the "menu" element that toggles the "fa-times" class and the "active" class on the "menu" and "navbar" elements, respectively, when clicked
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
+// Add an event listener to the window that removes the "fa-times" class and the "active" class from the "menu" and "navbar" elements, respectively, when scrolled
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
 
+// Create a new instance of the Swiper class with the options specified for the home slider
 var swiper = new Swiper(".home-slider", {
     grabCursor:true,
     loop:true,
@@ -25,6 +30,7 @@ var swiper = new Swiper(".home-slider", {
     },
 });
 
+// Create a new instance of the Swiper class with the options specified for the room slider
 var swiper = new Swiper(".room-slider", {
     spaceBetween: 20,
     grabCursor:true,
@@ -51,6 +57,7 @@ var swiper = new Swiper(".room-slider", {
     },
 });
 
+// Create a new instance of the Swiper class with the options specified for the gallery slider
 var swiper = new Swiper(".gallery-slider", {
     spaceBetween: 10,
     grabCursor:true,
@@ -77,6 +84,7 @@ var swiper = new Swiper(".gallery-slider", {
     },
 });
 
+// Create a new instance of the Swiper class with the options specified for the review slider
 var swiper = new Swiper(".review-slider", {
     spaceBetween: 10,
     grabCursor:true,
@@ -92,10 +100,13 @@ var swiper = new Swiper(".review-slider", {
     },
 });
 
+// Select all elements with the class "box" within the "content" element of each element with the class "row" within the element with the class "faqs", and assign them to the variable "accordions"
 let accordions = document.querySelectorAll('.faqs .row .content .box');
 
-accordions.forEach(acco =>{
-    acco.onclick = () =>{
+// Add an event listener to each "acco" element that removes the "active" class from all other "acco" elements,
+/ Loop through each accordion element and attach an onclick event listener.
+// When clicked, remove the 'active' class from all other accordion elements,
+ acco.onclick = () =>{
         accordions.forEach(subAcco => {subAcco.classList.remove('active')});
         acco.classList.add('active');
     }
